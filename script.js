589aa137724450480fafa3a0c7751f7c5ffc2352
webdeveloper-sonunarayansingh <webdevelopersonunarayansingh@gmail.com>
2023-07-02 13:42:43 +0530

    Script file added

---
+++ script.js
@@ -0,0 +1,27 @@
+const display = document.querySelector('#display')
+
+const button = document.querySelectorAll('button')
+
+
+
+button.forEach((item)=>{
+    item.onclick = ()=>{
+        if(item.id=='clear'){
+            display.innerText = ''
+        }
+        else if(item.id=='backspace'){
+            let string = display.innerText.toString()
+            display.innerText = string.substr(0, string.length-1)
+        }
+
+        else if(display.innerText!= '' && item.id == 'equal'){
+            display.innerText = eval(display.innerText)
+        }
+
+        else if(display.innerText=='' && item.id=='equal')
+            display.innerText = ('')
+        else{
+            display.innerText+= item.id
+        }
+    }
+})
